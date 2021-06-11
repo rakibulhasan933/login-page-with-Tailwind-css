c026e3f8e4db5ba835b1d75f6ea9d6baf92007ea
rakibulhasan933 <rakibulmd933@gmail.com>
2021-06-11 12:58:33 +0600

    final

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignInPage } from './components/SignInPage';
+
+function App() {
+  return (
+    <div className="App">
+      <SignInPage />
+    </div>
+  );
+}
+
+export default App;
